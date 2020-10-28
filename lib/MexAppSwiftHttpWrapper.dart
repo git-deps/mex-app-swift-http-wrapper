9de36ca7aa489bead893e40ac102b57389632236
Airon <airon@tark.pro>
2020-10-28 17:14:49 +0700

    MexAppSwiftHttpWrapper.dart: handle on Dart side responses from plugin
    https://dev.azure.com/DueDEX/Mex/_workitems/edit/1798/

--- lib/MexAppSwiftHttpWrapper.dart
+++ lib/MexAppSwiftHttpWrapper.dart
@@ -1,5 +1,5 @@
-
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
@@ -12,8 +12,55 @@ class MexAppSwiftHttpWrapper {
     return version;
   }
 
-  static Future get(String url) async {
-    final response = await _channel.invokeMethod('get', url);
+  static Future<Object> request(NetworkRequest request) async {
+    print('request - ' + jsonEncode(request.toJson()));
+    final Object response = await _channel.invokeMethod(
+      'request',
+      jsonEncode(request),
+    );
+    print('response - ' + response.toString());
     return response;
   }
+}
+
+class NetworkRequest {
+  String apiHost;
+  String endpoint;
+  String method;
+  Map<String, String> headers;
+  Map<String, dynamic> params;
+
+  NetworkRequest fromJson(Map<String, dynamic> json) => NetworkRequest()
+    ..apiHost = json['apiHost'] as String
+    ..endpoint = json['endpoint'] as String
+    ..method = json['method'] as String
+    ..headers = json['headers'] as Map<String, String>
+    ..params = json['params'] as Map<String, dynamic>;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'apiHost': apiHost,
+        'endpoint': endpoint,
+        'method': method,
+        'headers': headers,
+        'params': params,
+      };
+}
+
+class SwiftHttpError {
+  int networkErrorCode;
+  String invalidArgumentMessage;
+  String unknownErrorMessage;
+
+  SwiftHttpError();
+
+  factory SwiftHttpError.fromJson(Map<String, dynamic> json) => SwiftHttpError()
+    ..networkErrorCode = json['networkErrorCode'] as int
+    ..invalidArgumentMessage = json['invalidArgumentMessage'] as String
+    ..unknownErrorMessage = json['unknownErrorMessage'] as String;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'networkErrorCode': networkErrorCode,
+        'invalidArgumentMessage': invalidArgumentMessage,
+        'unknownErrorMessage': unknownErrorMessage,
+      };
 }
