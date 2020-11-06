05b393cf5fee5bdcd83e82156bb55c5914e73f4a
Airon <airon@tark.pro>
2020-11-06 10:52:44 +0700

    main.dart: improve the example code, set default timeout to 5, default retry to 1, remove getPlatformVersion
    https://dev.azure.com/DueDEX/Mex/_workitems/edit/1798/

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:MexAppSwiftHttpWrapper/MexAppSwiftHttpWrapper.dart';
 
 void main() {
@@ -20,33 +17,12 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    //initPlatformState();
     _getMarkets();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await MexAppSwiftHttpWrapper.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   _getMarkets() async {
-    await MexAppSwiftHttpWrapper.setTimeout(2.0);
-    await MexAppSwiftHttpWrapper.setRetryCount(2);
+    await MexAppSwiftHttpWrapper.setTimeout(5);
+    await MexAppSwiftHttpWrapper.setRetryCount(0);
     final response = await MexAppSwiftHttpWrapper.request(NetworkRequest()
       ..apiHost = 'https://app-api-int.duedex.com'
       ..endpoint = '/v1/instrument'
